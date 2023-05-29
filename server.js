44f184ab055ee354e1be6f6723ba458a3fa04f46
Lenny G Sempertegui C <lenny.sempertegui@uab.edu.bo>
2023-05-28 23:48:45 -0400

    Proyecto Inicial

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+// const chalk = require('chalk');
+const cors = require('cors');
+const app = express();
+
+
+app.use(cors());
+const options = {
+  cors: {
+    origin: 'http://localhost:4200',
+  },
+};
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server, options);
+
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+
+
+io.on('connection', function (socket) {
+
+  const handshake = socket.id;
+
+  let { nameRoom } = socket.handshake.query;
+  console.log(logrado);
+//   console.log(`${chalk.green(`Nuevo dispositivo: ${handshake}`)} conentado a la ${nameRoom}`);
+  socket.join(nameRoom)
+
+  socket.on('evento', (res) => {
+    // Emite el mensaje a todos lo miembros de las sala menos a la persona que envia el mensaje   
+    socket.to(nameRoom).emit('evento', res);
+
+  })
+
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(5000, function () {
+  console.log('\n')
+//   console.log(`>> Socket listo y escuchando por el puerto: ${chalk.green('5000')}`)
+})
